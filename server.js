c941e4bea27ba96c3343747ef440923a06f66725
harshiltech28@gmail.com <harshiltech28@gmail.com>
2019-02-16 15:24:07 +0530

    Posting with Postman

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 
 //Write to console
 console.log('Hello Web 2.0 App to Dapp');
@@ -9,11 +10,14 @@ app.get('/',(req,res) => {
   res.send("Hello Web 2.0 App to Dapp browser");
 });
 
+/* handling all the parsing */
+app.use(bodyParser.json());
+
 app.post('/',(req,res) => {
   var email = req.body.email;
   var amount = req.body.amount;
 
-  res.send({"amount" : amount,"email" : email})
+  res.send({'amount' : amount, 'email' : email})
 });
 
 var server = app.listen(3000, function () {
