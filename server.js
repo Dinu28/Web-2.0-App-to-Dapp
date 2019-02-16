9787438ae0ac300c94f6d1012881195db2a00ec5
harshiltech28@gmail.com <harshiltech28@gmail.com>
2019-02-16 19:49:17 +0530

    writing my first query

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-
+const {save_user_information} = require('./models/server_db');
 //Write to console
 console.log('Hello Web 2.0 App to Dapp');
 
@@ -13,18 +13,19 @@ app.get('/',(req,res) => {
 /* handling all the parsing */
 app.use(bodyParser.json());
 
-app.post('/',(req,res) => {
+app.post('/',async (req,res) => {
   var email = req.body.email;
   var amount = req.body.amount;
 
-  if (amount <=1){
+  if (amount <= 1){
     return_info = {};
     return_info.error = "true";
     return_info.message = "The Amount should be greater than 1";
     return res.send(return_info);
   }
-
-  res.send({'amount' : amount, 'email' : email})
+    var result = await save_user_information({"amount" : amount, "email" : email});
+    res.send(result);
+  //res.send({'amount' : amount, 'email' : email});
 });
 
 var server = app.listen(3000, function () {
